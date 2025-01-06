0edad4625f1bd0d5c2f36be1f43e40dcc9c5b483
SzymonDanza <szymonzlocki2001@gmail.com>
2025-01-06 17:40:34 +0100

    modele

---
+++ HotelSystem/Models/Reservation.cs
@@ -0,0 +1,9 @@
+﻿public class Reservation
+{
+    public int Id { get; set; }
+    public int RoomId { get; set; } 
+    public Room Room { get; set; } 
+    public string GuestName { get; set; } 
+    public DateTime CheckInDate { get; set; } 
+    public DateTime CheckOutDate { get; set; } 
+}
---
+++ HotelSystem/Models/Room.cs
@@ -0,0 +1,8 @@
+﻿public class Room
+{
+    public int Id { get; set; }
+    public string Name { get; set; } 
+    public int Capacity { get; set; } 
+    public decimal PricePerNight { get; set; } 
+    public ICollection<Reservation> Reservations { get; set; } 
+}
