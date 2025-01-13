9559fe75cfdc4d8dc2ee6c9499360a23e7f28ab0
SzymonDanza <szymonzlocki2001@gmail.com>
2025-01-13 22:28:50 +0100

    sss

--- HotelSystem/Models/Room.cs
+++ HotelSystem/Models/Room.cs
@@ -4,4 +4,4 @@
     {
     }
 }
-ssss
+sssssssss
--- HotelSystem/obj/Debug/net8.0/HotelSystem.AssemblyInfo.cs
+++ HotelSystem/obj/Debug/net8.0/HotelSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13e235feecab8c09a211e0f1dde066708578d71e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ce1d01b8a1da14c9140663cbc5a8a111aee4535")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
