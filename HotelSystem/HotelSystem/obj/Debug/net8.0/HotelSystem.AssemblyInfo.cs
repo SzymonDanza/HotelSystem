95f15117d88abd2ae2e166a35e1440ab1b28547e
SzymonDanza <szymonzlocki2001@gmail.com>
2025-01-13 21:09:11 +0100

    model Room/Reservation

--- HotelSystem/HotelSystem/obj/Debug/net8.0/HotelSystem.AssemblyInfo.cs
+++ HotelSystem/HotelSystem/obj/Debug/net8.0/HotelSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5469ca6ef62d7e86cccb9651165f6be805b7ee59")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
