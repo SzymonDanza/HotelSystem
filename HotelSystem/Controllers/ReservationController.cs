e6fcaa8379ba49688d1ede2c870057fbc11972ad
Wendel5 <michalwendel@interia.pl>
2025-01-14 18:35:50 +0100

    Save

--- HotelSystem/Controllers/ReservationController.cs
+++ HotelSystem/Controllers/ReservationController.cs
@@ -4,14 +4,14 @@ namespace HotelSystem.Controllers
 {
     public class ReservationController : Controller
     {
-        // Akcja dla strony głównej
+       
         public IActionResult Index()
         {
-            ViewData["Title"] = "Witamy w Rezerwatorze";  // Tytuł strony
-            return View("~/Views/Reservation/Index.cshtml");  // Zwróć widok 'Index'
+            ViewData["Title"] = "Witamy w Rezerwatorze";  
+            return View("~/Views/Reservation/Index.cshtml");  
         }
 
-        // Akcja dla strony polityki prywatności
+        
         public IActionResult Privacy()
         {
             return View();
